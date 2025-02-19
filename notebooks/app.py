828ca9724e4ce3dfeb49dfcfa1e27962c6dbceb5
Ayaz ahmed <ayazahmed824@gmail.com>
2025-02-19 08:05:10 +0000

    python program

--- notebooks/app.py
+++ notebooks/app.py
@@ -3,3 +3,12 @@ import pandas as pd
 import os
 from io import BytesIO
 
+st.set_page_config(page_title="Data Sweeper", layout="wide")
+st.title("DATA SWEEPER")
+st.write("Transfrom your files between CSV and Excel formats with built in data cleaning and visualization")
+
+uploaded_files = st.file_uploader("Upload your files (CSV or Excel);", type=["csv", "xlsx"], accept_multiple_files=True)
+
+if uploaded_files: 
+    for file in uploaded_files:
+        file_ext = os.path.splittext(file.name)[-1].lower()
